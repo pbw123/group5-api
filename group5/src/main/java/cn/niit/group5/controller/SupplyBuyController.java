de9974b605f5c38f39df436b0148b83cfdfd695c
pbw123 <2635225112@qq.com>
2019-05-16 12:37:06 +0800

    供求模块基本框架搭完

---
+++ group5/src/main/java/cn/niit/group5/controller/SupplyBuyController.java
@@ -0,0 +1,12 @@
+package cn.niit.group5.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Api(tags = "供求模块")
+@RestController
+@RequestMapping(value = "/api/supply")
+public class SupplyBuyController {
+
+}
---
+++ group5/src/main/java/cn/niit/group5/mapper/SupplyBuyMapper.java
@@ -0,0 +1,10 @@
+package cn.niit.group5.mapper;
+
+import cn.niit.group5.entity.SupplyBuy;
+
+import java.util.List;
+
+public interface SupplyBuyMapper {
+//供应列表
+    List<SupplyBuy>supplyList();
+}
