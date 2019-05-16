b76451bff95e7430df2d5b8b8777034d3a297896
pbw123 <2635225112@qq.com>
2019-05-16 13:07:47 +0800

    供求列表的接口初步完成

--- group5/src/main/java/cn/niit/group5/controller/SupplyBuyController.java
+++ group5/src/main/java/cn/niit/group5/controller/SupplyBuyController.java
@@ -1,10 +1,15 @@
 package cn.niit.group5.controller;
 
+import cn.niit.group5.entity.SupplyBuy;
 import cn.niit.group5.mapper.SupplyBuyMapper;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Api(tags = "供求模块")
 @RestController
@@ -13,7 +18,12 @@ public class SupplyBuyController {
 
     @Autowired
     private SupplyBuyMapper supplyBuyMapper;
-
+    @ApiOperation(value = "供应列表",notes = "暂时无法上传和显示图片")
+    @GetMapping(value = "supplyList")
+    public List<SupplyBuy> supplyBuysList()
+    {
+        return supplyBuyMapper.supplyList();
+    }
 
 
 }
--- group5/src/main/java/cn/niit/group5/mapper/SupplyBuyMapper.java
+++ group5/src/main/java/cn/niit/group5/mapper/SupplyBuyMapper.java
@@ -1,10 +1,14 @@
 package cn.niit.group5.mapper;
 
+import cn.niit.group5.entity.SupplyBuy;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Mapper
 @Component
 public interface SupplyBuyMapper {
-
+    //供应列表
+    List<SupplyBuy> supplyList();
 }
